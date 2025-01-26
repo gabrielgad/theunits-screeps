3223724489b99568d66cb1a2ee0a42739455a55d
Gabe Ronnie C <gabrielgad911@yahoo.com>
2025-01-26 08:34:41 -0500

    upgrades to builder body type

--- src/CreepBuilder.js
+++ src/CreepBuilder.js
@@ -12,7 +12,10 @@ class CreepBuilder {
         const bodies = [
             { cost: 200, body: [WORK, CARRY, MOVE] },
             { cost: 300, body: [WORK, CARRY, CARRY, MOVE, MOVE] },
-            { cost: 400, body: [WORK, WORK, CARRY, CARRY, MOVE, MOVE] }
+            { cost: 400, body: [WORK, WORK, CARRY, CARRY, MOVE, MOVE] },
+            { cost: 500, body: [WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE] },
+            { cost: 600, body: [WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE] },
+            { cost: 800, body: [WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE] }
         ];
 
         const selected = bodies
